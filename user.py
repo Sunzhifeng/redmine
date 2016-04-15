d97255530ee2320e61208c5d9c2c2006b2442f32
Zhifeng Sun <zhifeng.sun@ericsson.com>
2016-04-15 12:57:00 +0200

    my first practice
    Change-Id: I2bdff6658fe36e16caf8abb82419db37debf9430

---
+++ user.py
@@ -0,0 +1,7 @@
+
+class User(object):
+    def __init__(self, eId, username, password, email):
+        self.eId  = eId
+        self.username = username
+        self.password = password
+        self.email = email
