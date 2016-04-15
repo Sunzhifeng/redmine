93c9ccd13ed1f4851f6d3505befeda73eab72a7d
Zhifeng Sun <zhifeng.sun@ericsson.com>
2016-04-15 13:11:38 +0200

    test host_key
    Change-Id: I77eb9ad6e6ba136a1ff27fa346cea518e8611ec2

---
+++ manager.py
@@ -0,0 +1,9 @@
+
+class TicketHandler(object):
+    def assign_ticket(self, ticket, user):
+        ticket.assignee = user
+        print 'assign ticket %s  to %s ' % (ticket.title, user.username)
+
+    def release_code(self, user, ticket)
+        print ('{user.username} release ticket {ticket.title} code'.\
+               format(user = user, ticket = ticket))
