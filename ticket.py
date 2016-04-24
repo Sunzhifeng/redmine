75cb89395870729b52b2ed1003669965dfd2a96e
Zhifeng Sun <china-firstszf19892163.com>
2016-04-24 23:48:34 +0800

    rm unrelated files

--- ticket.py
+++
@@ -1,7 +0,0 @@
-
-class Ticket(object):
-    def __init__(self, tId, title, author, assignee=None):
-        self.tId = tId
-        self.title = title
-        self.author = author
-        self.assignee = assignee
--- user.py
+++
@@ -1,7 +0,0 @@
-
-class User(object):
-    def __init__(self, eId, username, password, email):
-        self.eId  = eId
-        self.username = username
-        self.password = password
-        self.email = email
